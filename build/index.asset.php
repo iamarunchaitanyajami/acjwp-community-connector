5c2122b9fbc4ae54e25296e6adc90137ed3a2ba9
iamarunchaitanyajami <iamarunchaitanyajami@gmail.com>
2024-02-06 14:44:17 +0000

    Update from https://github.com/arunchaitanyajami/acjwp-community-connector/commit/054ec473dfb6a8331da5171475961c5301e172ac

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-compose', 'wp-data'), 'version' => '694ebadf2889695ca9f9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => 'a8918df01689e5371075');
